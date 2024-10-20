f996becab7628487334481c2380c602154b5dc8b
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2024-10-20 21:45:17 +0200

    Se ha modificado el fichero index_modificado.php para añadirle una tabla de 10 personas, y en el caso que un usuario no tenga imagen hay un mensaje informativo y se puede visualizar a la persona igualmente.

--- index_modificado.php
+++ index_modificado.php
@@ -1,22 +1,67 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Llistat d'alumnes</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
 <body>
+    <header>
         <h1>Projecte Welcome 1</h1>
-        <ul>
-        <?php
-        $imgs = scandir("./img",SCANDIR_SORT_ASCENDING);
-        foreach( $imgs as $img ) {
-            if( $img=="." || $img==".." )
-                continue;
-            if( substr($img,-3)=="jpg" or substr($img,-3)=="png"){
-                $name = substr($img,0,-4);
-            }else if (substr($img,-4)=="jpeg") {
-                $name = substr($img,0,-5);
-            }
-            echo "<a href='profile/$name.html'>";
-            echo "<img src='img/$img' width='130'>";
-            echo $name."</a>";
-            echo "<div></div>";
+    </header>
+    
+    <main>
+        <section>
+            <table>
+                <tr>
+                <?php
+                $htmls = scandir('./profile', SCANDIR_SORT_ASCENDING);
+                $counter = 0;
 
-        }
-    ?>
-    </il>
+                foreach ($htmls as $html) {
+                    if ($html == "." || $html == "..") {
+                        continue;
+                    }
+
+                    $name = substr($html, 0, -5);
+
+                    $image_path = "";
+                    if (file_exists("./img/$name.jpg")) {
+                        $image_path = "./img/$name.jpg";
+                    } elseif (file_exists("./img/$name.png")) {
+                        $image_path = "./img/$name.png";
+                    } elseif (file_exists("./img/$name.jpeg")) {
+                        $image_path = "./img/$name.jpeg";
+                    }
+                
+                    echo "<td>";
+                    if (!$image_path) {
+                        echo "<div class='no-image'>Sense imatge</div>";
+                    }
+                    echo "<a href='profile/$name.html'>";
+                    if ($image_path) {
+                        echo "<img src='$image_path' alt='$name'>";
+                    }
+                    echo "<br>$name</a>";
+                    echo "</td>";
+
+                    $counter++;
+                    if ($counter % 10 == 0) {
+                        echo "</tr><tr>";
+                    }
+                }
+
+                if ($counter % 10 != 0) {
+                    echo "</tr>";
+                }
+                ?>
+            </table>
+        </section>
+    </main>
+    
+    <footer>
+        <p>&copy; 2024 Projecte Welcome 1</p>
+    </footer>
 </body>
+</html>
