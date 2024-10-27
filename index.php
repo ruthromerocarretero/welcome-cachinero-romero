6ada85bbda88cf3bd9c49274f10a814e5e06b73e
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2024-10-27 13:29:22 +0100

    Arreglar index.php y styles.css

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html lang="ca">
 <head>
     <meta charset="UTF-8">
@@ -7,13 +7,13 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
         <h1>Projecte Welcome 1</h1>
     </header>
-    
+
     <main>
         <section>
             <table>
@@ -38,10 +38,6 @@
                         $image_path = "./images/$name.jpeg";
                     }
 
-                    if ($counter % 10 == 0 && $counter != 0) {
-                        echo "</tr><tr>"; // Iniciar nueva fila cada 10 columnas
-                    }
-
                     echo "<td>";
                     if (!$image_path) {
                         echo "<div class='no-image'>Sense imatge</div>";
@@ -54,19 +50,21 @@
                     echo "</td>";
 
                     $counter++;
+                    if ($counter % 5 == 0) {
+                        echo "</tr><tr>";
+                    }
                 }
 
-                if ($counter % 10 != 0) {
-                    echo "</tr>"; // Cerrar última fila si no está completa
+                if ($counter % 5 != 0) {
+                    echo "</tr>";
                 }
                 ?>
             </table>
         </section>
     </main>
-    
+
     <footer>
     <p>&copy; 2024 Projecte Welcome 1., Ruth Romero y Marc Cachinero</p>
     </footer>
 </body>
 </html>
-
